e750fcab7c1423a95d758b9ace25862cffabe40d
Andre Kubotsu <contato201@gmail.com>
2021-11-18 14:40:20 -0300

    configuring fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
+import { query as q } from "faunadb"
+import { fauna } from "../../../services/fauna"
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -11,4 +14,18 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user) {
+
+      const { email } = user
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: {email}}
+        )
+      )
+      return true
+    },
+  }
 })
